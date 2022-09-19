bbc81cb2047c47da84cfd78ab4363c62664ceb7b
OPSJonathan <35155421+Jonny5-5@users.noreply.github.com>
2022-09-18 22:47:21 -0600

    working very well

---
+++ makeSignal.m
@@ -0,0 +1,11 @@
+function makeSignal(fr)
+    
+    % Default is 44.1 kHz because that's what phones use
+    % use a time length of 0.3 seconds because that's what we use for real
+    sampleRate = 44100;
+    t = 0:1/sampleRate:0.3;
+    wave = sin(2*pi*fr*t);
+    fileName = sprintf("%shz.wav", wave, sampleRate);
+    audiowrite(fileName, wave, sampleRate);
+    
+end
